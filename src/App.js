e997febbdd90e3842fd2ee6911cfde27f2370673
ldur <lasse@durucz.com>
2022-05-31 14:35:25 +0200

    second committ

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Lasse update 2 1 1
+          Edit <code>src/App.js</code> and save to reload. Lasse update 2 1 1 1
         </p>
         <a
           className="App-link"
